7a900ced00f19011c8fc152dbbf27582ce010f9d
Chedhlyg <chedhly@raeder-123.de>
2025-05-16 17:55:42 +0200

    SCSS-Dateien verschoben und Ordnerstruktur bereinigt

---
+++ .angular/cache/19.2.5/personalplanung-tool/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,49 @@
+import "./chunk-UDU42JBG.js";
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-2YAHLU3P.js";
+import "./chunk-ZJFEUAFE.js";
+import "./chunk-65RJ5ZZ2.js";
+import {
+  Dir
+} from "./chunk-PNN2A3NM.js";
+import "./chunk-YHPKCSZ5.js";
+import "./chunk-VJE4L4UK.js";
+import "./chunk-5SLB7SFU.js";
+import "./chunk-S35MAB2V.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
