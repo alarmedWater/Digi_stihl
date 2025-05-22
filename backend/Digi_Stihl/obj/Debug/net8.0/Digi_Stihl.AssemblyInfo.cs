d70075b846cc0a7c01b63be55f1572f6340fc6a8
Julian <julian.schrenk@stud.hs-hannover.de>
2025-05-22 21:21:23 +0200

    was passiert hier?

--- backend/Digi_Stihl/obj/Debug/net8.0/Digi_Stihl.AssemblyInfo.cs
+++ backend/Digi_Stihl/obj/Debug/net8.0/Digi_Stihl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digi_Stihl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429a5307245004099270081af82f18c8754c9618")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d754e439ebd5c646b39de0a5b380ef19734a423")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digi_Stihl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digi_Stihl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
