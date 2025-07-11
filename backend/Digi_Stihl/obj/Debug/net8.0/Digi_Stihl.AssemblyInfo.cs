8da9904d6a6da6689e3a6e459409ba9d9fb4e54c
Julian <julian.schrenk@stud.hs-hannover.de>
2025-07-11 12:56:40 +0200

    fixed menu bar

--- backend/Digi_Stihl/obj/Debug/net8.0/Digi_Stihl.AssemblyInfo.cs
+++ backend/Digi_Stihl/obj/Debug/net8.0/Digi_Stihl.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digi_Stihl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16150de30a46ce1e2280a65d1b861f98fb1897a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d61a75aa622d79b48f5a5509d727aef1f11d1da")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digi_Stihl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digi_Stihl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
